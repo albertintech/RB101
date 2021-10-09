886ff74373ad75f8edd55a59a5a6f772b3252a48
Albert Ramos <albertramos.dev@gmail.com>
2021-10-09 11:04:33 -0700

    Final calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -8,6 +8,26 @@
 
 Kernel.puts("Welcome to Calculator!")
 Kernel.puts("Please enter a first number: ")
-number1 = Kernel.gets()
+number1 = Kernel.gets().chomp()
 
-Kernel.puts("You have entered #{number1}")
+Kernel.puts("You have entered #{number1}.")
+
+Kernel.puts("Please enter a second number: ")
+number2 = Kernel.gets().chomp()
+
+Kernel.puts("You have entered #{number2}.")
+
+Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp()
+
+if operator == '1'
+  result = number1.to_i() + number2.to_i()
+elsif operator == '2'
+  result = number1.to_i() - number2.to_i()
+elsif operator == '3'
+  result = number1.to_i() * number2.to_i()
+else
+  result = number1.to_f() / number2.to_f()
+end
+
+Kernel.puts("The result is #{result}.")
