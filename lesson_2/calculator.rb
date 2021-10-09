c9ecba83cab1286bbd86e646c20fc4ded91afab3
Albert Ramos <albertramos.dev@gmail.com>
2021-10-09 16:46:19 -0700

    Refactor

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -6,28 +6,56 @@
 # answer = Kernel.gets()
 # Kernel.puts(answer)
 
-Kernel.puts("Welcome to Calculator!")
-Kernel.puts("Please enter a first number: ")
-number1 = Kernel.gets().chomp()
-
-Kernel.puts("You have entered #{number1}.")
-
-Kernel.puts("Please enter a second number: ")
-number2 = Kernel.gets().chomp()
-
-Kernel.puts("You have entered #{number2}.")
-
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
-
-if operator == '1'
-  result = number1.to_i() + number2.to_i()
-elsif operator == '2'
-  result = number1.to_i() - number2.to_i()
-elsif operator == '3'
-  result = number1.to_i() * number2.to_i()
-else
-  result = number1.to_f() / number2.to_f()
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-Kernel.puts("The result is #{result}.")
+def valid_number?(num)
+  num.to_i() != 0
+end
+
+prompt("Welcome to Calculator!")
+
+number1 = ''
+loop do
+  prompt("Please enter a first number: ")
+  number1 = Kernel.gets().chomp()
+
+  if valid_number?(number1)
+    break
+  else
+    prompt("Hmm... that does not look like a valid number!")
+  end
+
+end
+prompt("You have entered #{number1}.")
+
+number2 = ''
+loop do
+  prompt("Please enter a second number: ")
+  number2 = Kernel.gets().chomp()
+
+  if valid_number?(number2)
+    break
+  else
+    prompt("Hmm... that does not look like a valid number!")
+  end
+
+end
+prompt("You have entered #{number2}.")
+
+prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp()
+
+result =  case operator
+          when '1'
+            number1.to_i() + number2.to_i()
+          when '2'
+            number1.to_i() - number2.to_i()
+          when '3'
+            number1.to_i() * number2.to_i()
+          when '4'
+            number1.to_f() / number2.to_f()
+          end
+
+prompt("The result is #{result}.")
