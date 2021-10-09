e3b9dcf62a118dc853ee32af7115ced2da37d825
Albert Ramos <albertramos.dev@gmail.com>
2021-10-09 10:49:01 -0700

    Add calculator.rb

---
+++ lesson_2/calculator.rb
@@ -0,0 +1,13 @@
+# ask the user for two numbers
+# ask the user for an operation to perform
+# perform the operation on the two numbers
+# output the result
+
+# answer = Kernel.gets()
+# Kernel.puts(answer)
+
+Kernel.puts("Welcome to Calculator!")
+Kernel.puts("Please enter a first number: ")
+number1 = Kernel.gets()
+
+Kernel.puts("You have entered #{number1}")
