17d59ad52bf86b722f596d3f51ef9df8781f3051
Albert Ramos <albertramos.dev@gmail.com>
2021-10-09 16:56:22 -0700

    Add ask for name functionality

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -14,48 +14,70 @@ def valid_number?(num)
   num.to_i() != 0
 end
 
-prompt("Welcome to Calculator!")
+prompt("Welcome to Calculator! Enter your name: ")
 
-number1 = ''
+name = ''
 loop do
-  prompt("Please enter a first number: ")
-  number1 = Kernel.gets().chomp()
+  name = Kernel.gets().chomp()
 
-  if valid_number?(number1)
-    break
+  if name.empty?()
+    prompt("Make sure you use a valid name please.")
   else
-    prompt("Hmm... that does not look like a valid number!")
+    break
   end
+end
+
+prompt("Hello, #{name}.")
+
+# Main Loop
+loop do
+
+  number1 = ''
+  loop do
+    prompt("Please enter a first number: ")
+    number1 = Kernel.gets().chomp()
+
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm... that does not look like a valid number!")
+    end
+
+  end
+  prompt("You have entered #{number1}.")
+
+  number2 = ''
+  loop do
+    prompt("Please enter a second number: ")
+    number2 = Kernel.gets().chomp()
+
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm... that does not look like a valid number!")
+    end
+
+  end
+  prompt("You have entered #{number2}.")
+
+  prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+  operator = Kernel.gets().chomp()
+
+  result =  case operator
+            when '1'
+              number1.to_i() + number2.to_i()
+            when '2'
+              number1.to_i() - number2.to_i()
+            when '3'
+              number1.to_i() * number2.to_i()
+            when '4'
+              number1.to_f() / number2.to_f()
+            end
+
+  prompt("The result is #{result}.")
+
+  prompt("Do you want to perfom another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
 
 end
-prompt("You have entered #{number1}.")
-
-number2 = ''
-loop do
-  prompt("Please enter a second number: ")
-  number2 = Kernel.gets().chomp()
-
-  if valid_number?(number2)
-    break
-  else
-    prompt("Hmm... that does not look like a valid number!")
-  end
-
-end
-prompt("You have entered #{number2}.")
-
-prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
-
-result =  case operator
-          when '1'
-            number1.to_i() + number2.to_i()
-          when '2'
-            number1.to_i() - number2.to_i()
-          when '3'
-            number1.to_i() * number2.to_i()
-          when '4'
-            number1.to_f() / number2.to_f()
-          end
-
-prompt("The result is #{result}.")
