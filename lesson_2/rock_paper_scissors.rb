7ad8d85b2f5e0ff451c0b4b0121c037d4e352e42
Albert Ramos <albertramos.dev@gmail.com>
2021-10-11 15:24:25 -0700

    Add game loop

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -4,30 +4,36 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-choice = ''
 loop do
-  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-  choice = Kernel.gets().chomp()
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
 
-  if VALID_CHOICES.include?(choice)
-    break
-  else
-    prompt("That's not a valid choice.")
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
   end
-end
 
-computer_choice = VALID_CHOICES.sample()
+  computer_choice = VALID_CHOICES.sample()
 
-prompt("You chose #{choice}. Computer chose: #{computer_choice}.")
+  prompt("You chose #{choice}. Computer chose: #{computer_choice}.")
 
-if    (choice == 'rock' && computer_choice == 'scissors') ||
-      (choice == 'paper' && computer_choice == 'rock') ||
-      (choice == 'scissors' && computer_choice == 'paper')
-  prompt("You won!")
-elsif (choice == 'rock' && computer_choice == 'paper') ||
-      (choice == 'paper' && computer_choice == 'scissors') ||
-      (choice == 'scissors' && computer_choice == 'rock')
-  prompt("You lost!")
-else
-  prompt("Tie!")
+  if    (choice == 'rock' && computer_choice == 'scissors') ||
+        (choice == 'paper' && computer_choice == 'rock') ||
+        (choice == 'scissors' && computer_choice == 'paper')
+    prompt("You won!")
+  elsif (choice == 'rock' && computer_choice == 'paper') ||
+        (choice == 'paper' && computer_choice == 'scissors') ||
+        (choice == 'scissors' && computer_choice == 'rock')
+    prompt("You lost!")
+  else
+    prompt("Tie!")
+  end
+
+  prompt("Do you want to play again? (y/n)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
 end
