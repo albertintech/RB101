d9238a3548b584f28e4bb0e977390142632c88bf
Albert Ramos <albertramos.dev@gmail.com>
2021-10-11 15:42:02 -0700

    Put win/lose logic into method

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -3,14 +3,27 @@ VALID_CHOICES = ['rock','paper','scissors']
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
+def display_result(player, computer)
+  if    (player == 'rock' && computer == 'scissors') ||
+        (player == 'paper' && computer == 'rock') ||
+        (player == 'scissors' && computer == 'paper')
+    prompt("You won!")
+  elsif (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors') ||
+        (player == 'scissors' && computer == 'rock')
+    prompt("You lost!")
+  else
+    prompt("Tie!")
+  end
+end
 
 loop do
-  choice = ''
+  player_choice = ''
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
+    player_choice = Kernel.gets().chomp()
 
-    if VALID_CHOICES.include?(choice)
+    if VALID_CHOICES.include?(player_choice)
       break
     else
       prompt("That's not a valid choice.")
@@ -19,19 +32,9 @@ loop do
 
   computer_choice = VALID_CHOICES.sample()
 
-  prompt("You chose #{choice}. Computer chose: #{computer_choice}.")
+  prompt("You chose #{player_choice}. Computer chose: #{computer_choice}.")
 
-  if    (choice == 'rock' && computer_choice == 'scissors') ||
-        (choice == 'paper' && computer_choice == 'rock') ||
-        (choice == 'scissors' && computer_choice == 'paper')
-    prompt("You won!")
-  elsif (choice == 'rock' && computer_choice == 'paper') ||
-        (choice == 'paper' && computer_choice == 'scissors') ||
-        (choice == 'scissors' && computer_choice == 'rock')
-    prompt("You lost!")
-  else
-    prompt("Tie!")
-  end
+  display_result(player_choice, computer_choice)
 
   prompt("Do you want to play again? (y/n)")
   answer = Kernel.gets().chomp()
