69942860c83a37857e03b52b72441d563ba0ee36
Albert Ramos <albertramos.dev@gmail.com>
2021-10-12 14:27:26 -0700

    Exercise done

---
+++ lesson_3/easy2_q1.rb
@@ -0,0 +1,9 @@
+# In this hash of people and their age,
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+# see if "Spot" is present.
+
+p ages.include?("Spot")
+
+# also can use #key? and #member?
