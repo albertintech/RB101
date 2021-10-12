9b16bee47b4201da67f690105365091360d019af
Albert Ramos <albertramos.dev@gmail.com>
2021-10-12 12:32:03 -0700

    Exercise done

---
+++ lesson_3/easy1_q8.rb
@@ -0,0 +1,13 @@
+# Given the hash below
+
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+
+# Turn this into an array containing only two elements: Barney's name and Barney's number
+
+barney = flintstones.slice("Barney")
+
+p barney # Returns a hash with key and value
+
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+
+p flintstones.assoc("Barney") # Returns an array with key and value
