43be386c827ff1d0e856eb7ee5af562b17e5308d
Albert Ramos <albertramos.dev@gmail.com>
2021-10-12 14:51:47 -0700

    Exercise done

---
+++ lesson_3/easy2_q4.rb
@@ -0,0 +1,5 @@
+# See if the name "Dino" appears in the string below:
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+p advice.include?("Dino") # LS solution uses #match?
---
+++ lesson_3/easy2_q5.rb
@@ -0,0 +1,7 @@
+# Show an easier way to write this array:
+
+# flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+flintstones = %w(Fred, Barney, Wilma, Betty, BamBam)
+
+p flintstones
