7e98a5bd2348e82158f8ee6f46b497bdd8418b57
Albert Ramos <albertramos.dev@gmail.com>
2021-10-12 14:42:18 -0700

    Exercise done

---
+++ lesson_3/easy2_q3.rb
@@ -0,0 +1,8 @@
+# We have most of the Munster family in our age hash:
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
+
+# add ages for Marilyn and Spot to the existing hash
+additional_ages = { "Marilyn" => 22, "Spot" => 237 }
+
+p ages.merge!(additional_ages)
