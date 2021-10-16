94b84dfaba99ba4d776cccbf44c2f6e49fd422af
Albert Ramos <albertramos.dev@gmail.com>
2021-10-16 12:46:08 -0700

    Exercise done

---
+++ lesson_4/loops1_ex10.rb
@@ -0,0 +1,13 @@
+# Your friends just showed up! Given the following array of names, use a for loop to greet each friend individually.
+
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+# for i in friends do
+#   puts i
+# end
+
+# Read: for every 'i' element in 'x collection' do this for every i
+
+# The concise version:
+
+friends.each { |i| puts i}
