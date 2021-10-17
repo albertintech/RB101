d10d4f727e1297474ffd3ae20300ed18e8bc56f8
Albert Ramos <albertramos.dev@gmail.com>
2021-10-17 10:51:38 -0700

    Exercise done

---
+++ lesson_4/loops2_ex5.rb
@@ -0,0 +1,12 @@
+# Modify the code below so that the user's input gets added to the numbers array. Stop the loop when the array contains 5 numbers.
+
+numbers = []
+
+loop do
+  puts 'Enter any number:'
+  input = gets.chomp.to_i
+  numbers << input # or numbers.size(input)
+  break if numbers.size == 5
+end
+
+puts numbers
