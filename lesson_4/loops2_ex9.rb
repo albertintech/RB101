b99b8dc2c83d14cfa646feb385f99a57aebd56b7
Albert Ramos <albertramos.dev@gmail.com>
2021-10-17 11:16:31 -0700

    Exercise done

---
+++ lesson_4/loops2_ex9.rb
@@ -0,0 +1,29 @@
+# The following code increments number_a and number_b by either 0 or 1. loop is used so that the variables can be incremented more than once, however, break stops the loop after the first iteration. Use next to modify the code so that the loop iterates until either number_a or number_b equals 5. Print "5 was reached!" before breaking out of the loop.
+
+number_a = 0
+number_b = 0
+
+loop do
+  number_a += rand(2)
+  number_b += rand(2)
+  puts "Number A is #{number_a}"
+  puts "Number B is #{number_b}"
+
+  if (number_a == 5) || (number_b == 5)
+    puts "5 was reached!"
+    break
+  end
+
+end
+
+number_a = 0
+number_b = 0
+
+loop do
+  number_a += rand(2)
+  number_b += rand(2)
+  next unless number_a == 5 || number_b == 5
+
+  puts '5 was reached!'
+  break
+end
