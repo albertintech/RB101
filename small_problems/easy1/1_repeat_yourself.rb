2a36b73385fa46bbadac6619564e436cca88256c
Albert Ramos <albertramos.dev@gmail.com>
2021-10-19 09:51:06 -0700

    Add new directories and files

---
+++ small_problems/easy1/1_repeat_yourself.rb
@@ -0,0 +1,11 @@
+# Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the integer indicates.
+
+# Example:
+
+repeat('Hello', 3)
+
+# Output:
+
+Hello
+Hello
+Hello
---
+++ small_problems/easy1/2_odd.rb
@@ -0,0 +1,10 @@
+# Write a method that takes one integer argument, which may be positive, negative, or zero. This method returns true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
+
+# Examples
+
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
