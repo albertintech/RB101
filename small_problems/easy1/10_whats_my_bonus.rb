abeaeb1519d4db232a20cdbf125db92820c9e088
Albert Ramos <albertramos.dev@gmail.com>
2021-10-19 10:00:23 -0700

    Add problems

---
+++ small_problems/easy1/10_whats_my_bonus.rb
@@ -0,0 +1,9 @@
+# Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
+
+# Examples
+
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
+
+# The tests above should print true.
