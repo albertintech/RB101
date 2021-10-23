1805a646b94c0ea0a39da9b3e5cc984cb39d8e2c
Albert Ramos <72161221+albertintech@users.noreply.github.com>
2021-10-23 10:23:21 -0700

    Add parent_class.rb

---
+++ small_problems/documentation_again/parent_class.rb
@@ -0,0 +1,13 @@
+# Use irb to run the following code:
+
+s = 'abc'
+puts s.public_methods.inspect
+
+You should find that it prints a list of all of the public methods available to the String s;
+this includes not only those methods defined in the String class, but also methods that are
+inherited from Object (which inherits other methods from the BasicObject class and the Kernel module).
+That's a lot of methods.
+
+How would you modify this code to print just the public methods that are defined or overridden
+by the String class? That is, the list should exclude all members that are only defined in Object,
+BasicObject, and Kernel.
