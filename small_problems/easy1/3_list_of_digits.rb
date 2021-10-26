7ff17662d11c296bd35e7d7a3b3577e8c85f5dcf
Albert Ramos <albertramos.dev@gmail.com>
2021-10-26 06:59:57 -0700

    Exercise done

--- small_problems/easy1/3_list_of_digits.rb
+++ small_problems/easy1/3_list_of_digits.rb
@@ -1,6 +1,15 @@
+# Prereqs:
+# to_s
+# chars
+# map
+
 # Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
 
 # Examples:
+
+def digit_list(int)
+  int.to_s.chars.map(&:to_i)
+end
 
 puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
 puts digit_list(7) == [7]                     # => true
