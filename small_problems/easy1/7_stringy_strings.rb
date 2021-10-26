94c31ef98a9ca39809fbf3e15ba2360afeaed395
Albert Ramos <albertramos.dev@gmail.com>
2021-10-26 07:11:29 -0700

    Solution added

--- small_problems/easy1/7_stringy_strings.rb
+++ small_problems/easy1/7_stringy_strings.rb
@@ -8,3 +8,16 @@ puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
 
 # The tests above should print true.
+
+# LS Solution
+
+def stringy(size)
+  numbers = []
+
+  size.times do |index|
+    number = index.even? ? 1 : 0
+    numbers << number
+  end
+
+  numbers.join
+end
