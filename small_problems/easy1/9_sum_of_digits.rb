5632c599026b64b8abb83cf25e037b921c6525ae
Albert Ramos <albertramos.dev@gmail.com>
2021-10-26 07:22:27 -0700

    Add solution

--- small_problems/easy1/9_sum_of_digits.rb
+++ small_problems/easy1/9_sum_of_digits.rb
@@ -9,3 +9,24 @@ puts sum(123_456_789) == 45
 # The tests above should print true.
 #
 # For a challenge, try writing this without any basic looping constructs (while, until, loop, and each).
+
+# LS Solution 1
+
+def sum(number)
+  sum = 0
+  str_digits = number.to_s.chars
+
+  str_digits.each do |str_digit|
+    sum += str_digit.to_i
+  end
+
+  sum
+end
+
+# LS Solution 2
+# map
+# reduce
+
+def sum(number)
+  number.to_s.chars.map(&:to_i).reduce(:+)
+end
