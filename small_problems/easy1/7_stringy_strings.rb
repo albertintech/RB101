c04b9dfe174f8ba6a87dacc881583ba7df19e75f
Albert Ramos <albertramos.dev@gmail.com>
2021-10-26 07:16:01 -0700

    Add solution

--- small_problems/easy1/7_stringy_strings.rb
+++ small_problems/easy1/7_stringy_strings.rb
@@ -21,3 +21,13 @@ def stringy(size)
 
   numbers.join
 end
+
+# Discussion
+
+# Performing a task a certain number of times should be fairly straightforward at this point. We use #times to iterate based on the number indicated by size. For each iteration, we use the index block parameter in a conditional to determine if the current number is even or odd. Since #times starts counting from 0, we know that the first number will be even. Knowing this, we can write our conditional to return 1 if index is even and 0 if index is odd.
+
+# We assign that value to a variable and, on the next line, we add it to an array, numbers. After #times has finished iterating, we're left with an array filled with 1s and 0s in the correct order. Now, all we have to do is invoke numbers.join to return the desired output.
+
+# Further Exploration
+
+# Modify stringy so it takes an additional optional argument that defaults to 1. If the method is called with this argument set to 0, the method should return a String of alternating 0s and 1s, but starting with 0 instead of 1.
--- small_problems/easy1/8_array_average.rb
+++ small_problems/easy1/8_array_average.rb
@@ -1,3 +1,8 @@
+# Prereqs
+# hash
+# reduce
+
+
 # Write a method that takes one argument, an array containing integers, and returns the average of all numbers in the array. The array will never be empty and the numbers will always be positive integers. Your result should also be an integer.
 
 # Examples
@@ -6,4 +11,23 @@ puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
 puts average([1, 5, 87, 45, 8, 8]) == 25
 puts average([9, 47, 23, 95, 16, 52]) == 40
 
-The tests above should print true.
+# The tests above should print true.
+
+# LS Solution
+
+def average(numbers)
+  sum = numbers.reduce { |sum, number| sum + number }
+  sum / numbers.count
+end
+
+# Discussion
+
+# Two things need to be done to find the average. First, add every number together. Second, divide the sum by the number of elements. We accomplish the first part by using Enumerable#reduce(also known as #inject), which combines all elements of the given array by applying a binary operation. This operation is specified by a block or symbol. We used a block in our solution, but we could have just as easily used a symbol, like this:
+
+numbers.reduce(:+)
+
+# Once we have the sum, all that's left is to divide it by the number of elements. To do that, we use #count to count the number of elements in numbers. Then, we divide sum by the number of elements and return the quotient.
+
+# Further Exploration
+
+# Currently, the return value of average is an Integer. When dividing numbers, sometimes the quotient isn't a whole number, therefore, it might make more sense to return a Float. Can you change the return value of average from an Integer to a Float?
