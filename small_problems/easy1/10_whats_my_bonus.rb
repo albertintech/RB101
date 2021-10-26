e91322435403686c4efadc2c4ea9fcd73257f6e2
Albert Ramos <albertramos.dev@gmail.com>
2021-10-26 07:19:24 -0700

    Add solution

--- small_problems/easy1/10_whats_my_bonus.rb
+++ small_problems/easy1/10_whats_my_bonus.rb
@@ -1,3 +1,6 @@
+# Prereqs
+# ternary operator
+
 # Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
 
 # Examples
@@ -7,3 +10,13 @@ puts calculate_bonus(1000, false) == 0
 puts calculate_bonus(50000, true) == 25000
 
 # The tests above should print true.
+
+# LS Solution
+
+def calculate_bonus(salary, bonus)
+  bonus ? (salary / 2) : 0
+end
+
+# Discussion
+
+# The solution to this exercise takes advantage of the ternary operator. We're able to use bonus as the condition because it's a boolean, which means its value will only be true or false. If it's true, then we divide salary by 2 and return the quotient. If it's false, then we return 0.
