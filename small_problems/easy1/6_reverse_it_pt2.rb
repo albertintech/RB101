d27ef47a082a5c15f1927ca43b9d623dffb65985
Albert Ramos <albertramos.dev@gmail.com>
2021-10-26 07:09:05 -0700

    Prereqs added

--- small_problems/easy1/6_reverse_it_pt2.rb
+++ small_problems/easy1/6_reverse_it_pt2.rb
@@ -1,3 +1,10 @@
+# Prereqs
+# split
+# size
+# reverse!
+# <<
+# join(' ')
+
 # Write a method that takes one argument, a string containing one or more words, and returns the given string with words that contain five or more characters reversed. Each string will consist of only letters and spaces. Spaces should be included only when more than one word is present.
 
 # Examples
