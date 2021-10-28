2a8238cf96cc72eb9d9579a2605fb26cb272801c
Albert Ramos <albertramos.dev@gmail.com>
2021-10-28 12:36:38 -0700

    Exercise done

--- lesson_4/topic_10_practice_problems/1.rb
+++ lesson_4/topic_10_practice_problems/1.rb
@@ -4,3 +4,15 @@
   num > 5
   'hi'
 end
+
+# Answer: [1, 2, 3]. Why? Becuase the return value of the block is a string, which is truthy, the return value of select is a new array with each truthy value, ie all values in this case.
+
+# Ruby Docs:
+# select {|item| block} → new_ary
+# select → Enumerator
+# Returns a new array containing all elements of ary for which the given block returns a true value.
+#
+# If no block is given, an Enumerator is returned instead.
+
+#LS Solution:
+# select performs selection based on the truthiness of the block's return value. In this case the return value will always be 'hi', which is a "truthy" value. Therefore select will return a new array containing all of the elements in the original array.
