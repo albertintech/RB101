1fbdb29d2e90dffb95a3ad265545a03ba9141b26
Albert Ramos <albertramos.dev@gmail.com>
2021-10-28 12:25:34 -0700

    Exercise done

--- lesson_4/topic_10_practice_problems/2.rb
+++ lesson_4/topic_10_practice_problems/2.rb
@@ -1,5 +1,26 @@
 # How does count treat the block's return value? How can we find out?
 
-['ant', 'bat', 'caterpillar'].count do |str|
+new = ['ant', 'bat', 'caterpillar'].count do |str|
   str.length < 4
 end
+
+p new
+
+# => 2
+
+# Ruby docs:
+# count → int
+# count(obj) → int
+# count {|item| block} → int
+# Returns the number of elements.
+#
+# If an argument is given, counts the number of elements which equal obj using ==.
+#
+# If a block is given, counts the number of elements for which the block returns a true value.
+
+# LS Solution:
+# If we don't know how count treats the block's return value, then we should consult the docs for Array#count. Our answer is in the description:
+#
+# If a block is given, counts the number of elements for which the block returns a true value.
+#
+# Based on this information, we can conclude that count only counts an element if the block's return value evaluates as true. This means that count considers the truthiness of the block's return value.
