b3031113d04bd670d12a2feac9b0b8124a94ab6d
Albert Ramos <albertramos.dev@gmail.com>
2021-10-28 13:21:01 -0700

    Exercise done

--- lesson_4/topic_10_practice_problems/10.rb
+++ lesson_4/topic_10_practice_problems/10.rb
@@ -7,3 +7,8 @@
     num
   end
 end
+
+# => [1,nil,nil]
+
+#LS Solution
+# We can determine the block's return value by looking at the return value of the last statement evaluated within the block. In this case it's a bit tricky because of the if statement. For the first element, the if condition evaluates as false, which means num is the block's return value for that iteration. For the rest of the elements in the array, num > 1 evaluates as true, which means puts num is the last statement evaluated, which in turn, means that the block's return value is nil for those iterations.
