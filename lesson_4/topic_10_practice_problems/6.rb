6d2ec4b7c129bf56d16bce5646740c7841e991fb
Albert Ramos <albertramos.dev@gmail.com>
2021-10-28 13:42:09 -0700

    Exercise done

--- lesson_4/topic_10_practice_problems/6.rb
+++ lesson_4/topic_10_practice_problems/6.rb
@@ -1,3 +1,7 @@
 # What is the return value of the following statement? Why?
 
 ['ant', 'bear', 'caterpillar'].pop.size
+
+# => 11
+
+# There are a couple things going on here. First, pop is being called on the array. pop destructively removes the last element from the calling array and returns it. Second, size is being called on the return value by pop. Once we realize that size is evaluating the return value of pop (which is "caterpillar") then the final return value of 11 should make sense.
