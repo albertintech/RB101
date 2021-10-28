155b50f2fca4f35d2a11b5e8265f5f16b5430cff
Albert Ramos <albertramos.dev@gmail.com>
2021-10-28 13:33:55 -0700

    Exercise done

--- lesson_4/topic_10_practice_problems/8.rb
+++ lesson_4/topic_10_practice_problems/8.rb
@@ -2,3 +2,21 @@
 
 arr = [1, 2, 3, 4, 5]
 arr.take(2)
+
+# >> arr.take(2)
+# => [1, 2]
+
+# take is non-destructive
+# >> arr
+# => [1, 2, 3, 4, 5]
+
+# Ruby docs
+# take(n) → new_ary
+# Returns first n elements from the array.
+#
+# If a negative number is given, raises an ArgumentError.
+#
+# See also Array#drop
+
+# a = [1, 2, 3, 4, 5, 0]
+# a.take(3)             #=> [1, 2, 3]
