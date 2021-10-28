a4496c5768143fc46c1b06ba8d565403054fdd80
Albert Ramos <albertramos.dev@gmail.com>
2021-10-28 13:25:39 -0700

    Exercise done

--- lesson_4/topic_10_practice_problems/9.rb
+++ lesson_4/topic_10_practice_problems/9.rb
@@ -5,3 +5,11 @@
     value
   end
 end
+
+# => ['bear']??? Incorrect
+
+# => [nil, "bear"] Becuase when none of the conditions in an if statement evaluate as true, the if statement returns nil.
+
+# There are some interesting things to point out here. First, the return value of map is an array, which is the collection type that map always returns. Second, where did that nil come from? If we look at the if condition (value.size > 3), we'll notice that it evaluates as true when the length of value is greater than 3. In this case, the only value with a length greater than 3 is 'bear'. This means that for the first element, 'ant', the condition evaluates as false and value isn't returned.
+
+# When none of the conditions in an if statement evaluates as true, the if statement itself returns nil. That's why we see nil as the first element in the returned array.
