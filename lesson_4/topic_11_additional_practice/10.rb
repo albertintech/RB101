24ed60548afe2b885c1de9e5e7c356f7ebe8a576
Albert Ramos <albertramos.dev@gmail.com>
2021-10-29 09:17:08 -0700

    Line up hash values

--- lesson_4/topic_11_additional_practice/10.rb
+++ lesson_4/topic_11_additional_practice/10.rb
@@ -1,10 +1,10 @@
 # Given the munsters hash below
 
 munsters = {
-  "Herman" => { "age" => 32, "gender" => "male" },
-  "Lily" => { "age" => 30, "gender" => "female" },
+  "Herman"  => { "age" => 32, "gender" => "male" },
+  "Lily"    => { "age" => 30, "gender" => "female" },
   "Grandpa" => { "age" => 402, "gender" => "male" },
-  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Eddie"   => { "age" => 10, "gender" => "male" },
   "Marilyn" => { "age" => 23, "gender" => "female"}
 }
 
