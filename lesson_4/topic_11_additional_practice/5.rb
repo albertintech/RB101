371c2509b6c2f4380ce673932b6bc5cbec4ebab7
Albert Ramos <albertramos.dev@gmail.com>
2021-10-30 13:49:37 -0700

    Exercise done

--- lesson_4/topic_11_additional_practice/5.rb
+++ lesson_4/topic_11_additional_practice/5.rb
@@ -2,6 +2,24 @@
 
 flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
 
-# Find the index of the first name that starts with "Be"
+# Find the index of the first name that starts with "Bam"
 
-flintstones.index { |name| name[0, 2] == "Be" }
+p flintstones.index { |name| name[0, 3] == "Bam" }
+
+# Array#index
+
+# index(obj) → int or nil
+# index {|item| block} → int or nil
+# index → Enumerator
+# Returns the index of the first object in ary such that the object is == to obj.
+#
+# If a block is given instead of an argument, returns the index of the first object for which the block returns true. Returns nil if no match is found.
+#
+# See also Array#rindex.
+#
+# An Enumerator is returned if neither a block nor argument is given.
+#
+# a = [ "a", "b", "c" ]
+# a.index("b")              #=> 1
+# a.index("z")              #=> nil
+# a.index {|x| x == "b"}    #=> 1
