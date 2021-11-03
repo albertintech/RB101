4bcce88b4bfb0c0d76c6cb211678d9608dfccb30
Albert Ramos <albertramos.dev@gmail.com>
2021-11-03 16:48:28 -0700

    Exercise done

---
+++ lesson_5/topic_5_practice_problems/5.rb
@@ -0,0 +1,15 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+total_male_age = 0
+
+munsters.each_value do |details|
+  total_male_age += details["age"] unless details["gender"] == "female"
+end
+
+p total_male_age
