ff36a8e2be826f6b540a1f135be96af1ee657026
Albert Ramos <albertramos.dev@gmail.com>
2021-11-03 14:30:59 -0700

    Exercise done

--- lesson_5/topic_5_practice_problems/2.rb
+++ lesson_5/topic_5_practice_problems/2.rb
@@ -8,3 +8,7 @@ books = [
 ]
 
 p books.sort_by { |book| book[:published] }
+
+# We can use sort_by and access a particular value in each hash by which to sort, as long as those values have a <=> method.
+
+# In this case, all the values for :published are strings so we know that we can compare them. Since all the values in question are four characters in length, in this case we can simply compare the strings without having to convert them to integers.
