63f2eb097e8a0e9e76a57caef171547157092732
Albert Ramos <albertramos.dev@gmail.com>
2021-11-03 21:59:25 -0700

    Add exercises

---
+++ lesson_5/topic_5_practice_problems/13.rb
@@ -0,0 +1,7 @@
+# Given the following data structure, return a new array containing the same sub-arrays as the original but ordered logically by only taking into consideration the odd numbers they contain.
+
+arr = [[1, 6, 7], [1, 4, 9], [1, 8, 3]]
+
+# The sorted array should look like this:
+
+[[1, 8, 3], [1, 6, 7], [1, 4, 9]]
---
+++ lesson_5/topic_5_practice_problems/15.rb
@@ -0,0 +1,3 @@
+# Given this data structure write some code to return an array which contains only the hashes where all the integers are even.
+
+arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
