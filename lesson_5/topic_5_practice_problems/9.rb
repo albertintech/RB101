3d74be30a46964a6897b1ff936a0b1cff5fa6c9b
Albert Ramos <albertramos.dev@gmail.com>
2021-11-03 17:58:42 -0700

    Add exercises

---
+++ lesson_5/topic_5_practice_problems/9.rb
@@ -0,0 +1,3 @@
+# Given this data structure, return a new array of the same structure but with the sub arrays being ordered (alphabetically or numerically as appropriate) in descending order.
+
+arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']]
