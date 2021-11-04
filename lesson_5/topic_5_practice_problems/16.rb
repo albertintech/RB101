1914552194a5dc31a4098bb73ec9e76b4484f1a1
Albert Ramos <albertramos.dev@gmail.com>
2021-11-03 22:00:50 -0700

    Ridiculous question

--- lesson_5/topic_5_practice_problems/16.rb
+++ lesson_5/topic_5_practice_problems/16.rb
@@ -7,3 +7,20 @@
 # It looks like this: "f65c57f6-a6aa-17a8-faa1-a67f2dc9fa91"
 #
 # Write a method that returns one UUID when called with no parameters.
+
+def generate_UUID
+  characters = []
+  (0..9).each { |digit| characters << digit.to_s }
+  ('a'..'f').each { |digit| characters << digit }
+
+  uuid = ""
+  sections = [8, 4, 4, 4, 12]
+  sections.each_with_index do |section, index|
+    section.times { uuid += characters.sample }
+    uuid += '-' unless index >= sections.size - 1
+  end
+
+  uuid
+end
+
+# Uh, yeah right buddy. What a freaking joke of a question.
