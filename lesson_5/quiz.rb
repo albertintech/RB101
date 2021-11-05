dbfa7c9403ffdb4a919658679d2938b11a4dba90
Albert Ramos <albertramos.dev@gmail.com>
2021-11-05 11:21:12 -0700

    Add lines

--- lesson_5/quiz.rb
+++ lesson_5/quiz.rb
@@ -67,3 +67,73 @@
 #     letter.upcase
 #   end
 # end
+
+order_data = [
+  {customer_id: 12, customer_name: 'Emma Lopez', order_id: 351, order_date: '12/04/16', order_fulfilled: true, order_value: 135.99},
+  {customer_id: 12, customer_name: 'Emma Lopez', order_id: 383, order_date: '12/04/16', order_fulfilled: true, order_value: 289.49},
+  {customer_id: 12, customer_name: 'Emma Lopez', order_id: 392, order_date: '01/10/17', order_fulfilled: false, order_value: 58.00},
+  {customer_id: 32, customer_name: 'Michael Richards', order_id: 241, order_date: '11/10/16', order_fulfilled: true, order_value: 120.00},
+  {customer_id: 32, customer_name: 'Michael Richards', order_id: 395, order_date: '01/10/17', order_fulfilled: false, order_value: 85.65}
+]
+
+customer_orders = {}
+
+order_data.each do |row|
+  if customer_orders.key?(row[:customer_id])
+    customer_orders[row[:customer_id]][:orders] << {
+      order_fulfilled: row[:order_fulfilled],
+      order_value: row[:order_value]
+    }
+  else
+    customer_orders[row[:customer_id]] = {
+      customer_id: row[:customer_id],
+      customer_name: row[:customer_name],
+      orders: [
+        {
+          order_fulfilled: row[:order_fulfilled],
+          order_value: row[:order_value]
+        }
+      ]
+    }
+  end
+end
+
+customer_orders = customer_orders.values
+
+all_orders = []
+
+customer_orders.each do |customer_data|
+  cust_arr = [
+    [:customer_id, customer_data[:customer_id]],
+    [:customer_name, customer_data[:customer_name]],
+    [:total_order_value]
+  ]
+
+  order_value = 0
+  customer_data[:orders].each do |order|
+    order_value += order[:order_value]
+  end
+
+  cust_arr[2] << order_value
+
+  all_orders << cust_arr
+end
+
+p all_orders
+
+fulfilled_orders = customer_orders.map do |customer|
+  {
+    customer_id: customer[:customer_id],
+    customer_name: customer[:customer_name]
+  }
+end
+
+customer_orders.each_with_index do |data, index|
+  order_value = data[:orders].reduce(0) do |total, order|
+    total + order[:order_value] if order[:order_fulfilled]
+  end
+
+  fulfilled_orders[index][:order_value] = order_value
+end
+
+p fulfilled_orders
