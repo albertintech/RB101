3453f430e51affb07cc7955c909dbc7166651af9
Albert Ramos <albertramos.dev@gmail.com>
2021-11-04 20:10:25 -0700

    Exercise done

--- lesson_5/topic_5_practice_problems/13.rb
+++ lesson_5/topic_5_practice_problems/13.rb
@@ -4,4 +4,17 @@ arr = [[1, 6, 7], [1, 4, 9], [1, 8, 3]]
 
 # The sorted array should look like this:
 
-[[1, 8, 3], [1, 6, 7], [1, 4, 9]]
+# [[1, 8, 3], [1, 6, 7], [1, 4, 9]]
+
+
+arr.sort_by do |sub_arr|
+  sub_arr.select do |num|
+    num.odd?
+  end
+end
+# => [[1, 8, 3], [1, 6, 7], [1, 4, 9]]
+
+# LS Solution
+# Since the sub-arrays are compared in an 'element-wise' manner when being sorted, when looking at the first element of each they are all equal. If we were to include the second element of each (the even integers) in our comparison the order would be different, since 4 is less than 6, which is less than 8.
+
+# By performing selection on the sub-arrays that we are comparing, we can compare them based on the value of the odd integers alone.
