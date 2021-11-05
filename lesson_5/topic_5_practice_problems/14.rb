aa59e799ff2349cc7eeb8ddfd4fc7bbc28b73052
Albert Ramos <albertramos.dev@gmail.com>
2021-11-04 20:13:57 -0700

    Exercise done

--- lesson_5/topic_5_practice_problems/14.rb
+++ lesson_5/topic_5_practice_problems/14.rb
@@ -11,3 +11,22 @@ hsh = {
 # The return value should look like this:
 
 [["Red", "Green"], "MEDIUM", ["Red", "Green"], ["Orange"], "LARGE"]
+
+
+# LS Solution
+hsh.map do |_, value|
+  if value[:type] == 'fruit'
+    value[:colors].map do |color|
+      color.capitalize
+    end
+  elsif value[:type] == 'vegetable'
+    value[:size].upcase
+  end
+end
+# => [["Red", "Green"], "MEDIUM", ["Red", "Green"], ["Orange"], "LARGE"]
+
+# From the problem description we know that we want to return an array where each item in the outer hash is represented by a particular value from within each nested hash, so map is a good choice of method to call on the initial hsh object.
+
+# Since the value to be returned to map by the outer block depends on whether :type equates to fruit or vegetable, we need to use a conditional statement to determine what is returned and what other actions are performed on those values prior to them being returned by the outer block.
+
+# The situation for :colors is complicated slightly in that we want to call capitalize on the strings but they are nested within arrays so we need to iterate through those arrays in order to access them.
