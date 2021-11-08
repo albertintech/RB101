541ca2cf607628bbebf7bb5a9108ab0904ca188d
Albert Ramos <albertramos.dev@gmail.com>
2021-11-08 12:03:36 -0700

    Update file

--- lesson_6/topic_3_debugging_with_pry/scratchpad.rb
+++ lesson_6/topic_3_debugging_with_pry/scratchpad.rb
@@ -9,14 +9,14 @@ require 'pry'
 #   puts 'It\'s true!'
 # end
 
-puts "Please pick an option: 1 or 2"
-user_input = gets.chomp
-p user_input
-# binding.pry
-if user_input.to_i == 1
-  puts "You picked option 1"
-elsif user_input == '2'
-  puts "You picked option 2"
-else
-  puts "Invalid option!"
-end
+# puts "Please pick an option: 1 or 2"
+# user_input = gets.chomp
+# p user_input
+# # binding.pry
+# if user_input.to_i == 1
+#   puts "You picked option 1"
+# elsif user_input == '2'
+#   puts "You picked option 2"
+# else
+#   puts "Invalid option!"
+# end
