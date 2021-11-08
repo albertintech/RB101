42755ab9c3f323c9599dc2ad1c69d07e1d2d46a3
Albert Ramos <albertramos.dev@gmail.com>
2021-11-08 12:28:15 -0700

    Update file

--- small_problems/easy1/4_how_many.rb
+++ small_problems/easy1/4_how_many.rb
@@ -21,3 +21,51 @@ car => 4
 truck => 3
 SUV => 1
 motorcycle => 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# LS Solution
+# def count_occurrences(array)
+#   occurrences = {}
+#
+#   array.uniq.each do |element|
+#     occurrences[element] = array.count(element)
+#   end
+#
+#   occurrences.each do |element, count|
+#     puts "#{element} => #{count}"
+#   end
+# end
+
+# Discussion
+# As we iterate over each unique element, we create a new key-value pair in occurrences, with the key as the element's value. To count each occurrence, we use Array#count to count the number of elements with the same value.
+#
+# Lastly, to print the desired output, we call #each on the newly created occurrences, which lets us pass the keys and values as block parameters. Then, inside of the block, we invoke #puts to print each key-value pair.
--- small_problems/easy1/5_reverse_it_pt1.rb
+++ small_problems/easy1/5_reverse_it_pt1.rb
@@ -13,3 +13,54 @@ puts reverse_sentence('') == ''
 puts reverse_sentence('    ') == '' # Any number of spaces results in ''
 
 # The tests above should print true.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# LS Solution
+# def reverse_sentence(string)
+#   string.split.reverse.join(' ')
+# end
+
+# Discussion
+# To reverse the order of substrings within a string, we first need to separate those substrings. In our solution, we use #split with no arguments to separate each word and place it in an array. To reverse the order of the words, we then invoke #reverse on the array. Now, string looks like this:
+#
+# 'Hello World'.split.reverse # => ["World", "Hello"]
+#
+# That's precisely what we want except we need it to be a string, not an array. To accomplish this, we invoke #join which joins every element in an array using the given argument as the delimiter.
+#
+# Note that we don't need to do anything special to handle the last two tests. Since split splits on whitespace when invoked without an argument, both '' and ' ' cause split to return an empty array.
