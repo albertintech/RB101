ba01eef20f9017aa2e4dde3f3a56cfc93fedec71
Albert Ramos <albertramos.dev@gmail.com>
2021-11-30 07:21:48 -0700

    Add twenty_one.rb

---
+++ lesson_6/twenty_one.rb
@@ -0,0 +1,9 @@
+# 1. Initialize deck
+# 2. Deal cards to player and dealer
+# 3. Player turn: hit or stay
+#   - repeat until bust or "stay"
+# 4. If player bust, dealer wins.
+# 5. Dealer turn: hit or stay
+#   - repeat until total >= 17
+# 6. If dealer bust, player wins.
+# 7. Compare cards and declare winner.
